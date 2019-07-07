960ff3fbeffdc46efd3f7ceb2e6a72c49dfca690
Darrin Spell Jr <dspell74@gmail.com>
2019-07-07 09:09:14 -0700

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,6 +6,28 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
+
+    /**
+     * Display phrase on game board
+     */
+    addPhraseToDisplay() {
+        const ul = document.querySelector("ul");
+        const letter = this.phrase;
+
+        for (let letter of this.phrase) {
+            if (letter === " ") {
+                const li = document.createElement("li");
+                li.classList.add('space');
+                ul.appendChild(li);
+            } else {
+                const li = document.createElement("li");
+                li.textContent = letter;
+                li.classList.add('hide', 'letter', letter);
+                ul.appendChild(li);
+            }
+        }
+
+    };
 }
 
 // const phrase = new Phrase();
