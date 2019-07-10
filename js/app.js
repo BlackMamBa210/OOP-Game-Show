56d355f717db3b14910a477f29d9e34b8deaf5d1
Darrin Spell Jr <dspell74@gmail.com>
2019-07-10 06:57:23 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -29,3 +29,9 @@
 // const game = new Game();
 // game.startGame();
 // console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+
+const game = new Game();
+
+btn__reset.addEventListener('click', (e) => {
+    game.startGame();
+})
