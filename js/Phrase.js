4d36a60d918b943f3d1068a457b18b7bcf21457c
Darrin Spell Jr <dspell74@gmail.com>
2019-07-14 14:41:16 -0700

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -29,6 +29,29 @@ class Phrase {
         }
 
     };
-}
 
-// const phrase = new Phrase();
+    /**
+     * Checks if passed letter is in phrase
+     * @param (string) letter - Letter to check
+     */
+    checkLetter(letter) {
+        if ([...this.phrase].indexOf(letter) !== -1) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+    /**
+     * Displays passed letter on screen after a match is found
+     * @param (string) letter - Letter to display
+     */
+    showMatchedLetter(letter) {
+        const list = document.querySelectorAll('li');
+
+        for (let li of list) {
+            if (li.textContent === letter) {
+                li.classList.add('show');
+            }
+        }
+    };
+};
