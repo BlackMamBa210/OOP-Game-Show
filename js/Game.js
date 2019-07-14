0f8d8c83dcf5def9e662e2b59acaaa98610817ca
Darrin Spell Jr <dspell74@gmail.com>
2019-07-14 01:43:04 -0700

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -49,6 +49,53 @@ class Game {
 
         console.log(`Active Phrase - phrase: ${game.activePhrase}`);
     };
+
+    handleInteraction(KeyboardEvent) {
+        KeyboardEvent.diabled = true;
+
+        if (phrase.checkLetter(key.textContent)) {
+            key.classList += 'chosen';
+            phrase.showMatchedLetter(key.textContent);
+            this.gameOver;
+        } else {
+            key.classList += 'wrong';
+            this.removeLife();
+            this.missed++;
+            this.gameOver();
+        }
+        //console.log(handleInteraction());
+    }
+
+
+    /**
+     * Checks for winning move
+     * @return {boolean} True if game has been won, false if game wasn't won
+     */
+    checkForWin() {
+        const phraseLetters = [...this.activePhrase];
+        const phraseCount = letters.filter(element => element !== ' ').length;
+        const shownPhraseLetters = document.querySelectorAll('.show').length;
+
+        if (shownPhraseLetters === phraseCount) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    /**
+     * Increases the value of the missed property
+     * Removes a life from the scoreboard
+     * Checks if player has remaining lives and ends game if player is out
+     */
+    removeLife() {};
+
+    /**
+     * Displays game over message
+     * @param {boolean} gameWon - Whether or not the user won the game
+     */
+    gameOver(gameWon) {};
+
 }
 
 // const game = new Game();
