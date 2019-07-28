36932e3b6d9d9c77955d1425e61748cb84ef39dc
Darrin Spell Jr <dspell74@gmail.com>
2019-07-27 19:09:24 -0700

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -13,59 +13,53 @@ class Game {
      * Creates phrases for use in game
      * @return {array} An array of phrases that could be used in the game
      */
+
     createPhrases() {
-        const phrases = [
+        let phrases = [
             new Phrase("Life is like a box of chocolates"),
             new Phrase("There is no trying"),
             new Phrase("May the force be with you"),
             new Phrase("You have to see the matrix for yourself"),
             new Phrase("You talking to me")
         ];
-
-        return phrases;
-    };
+        return phrases
+    }
 
     /**
      * Selects random phrase from phrases property
      * @return {Object} Phrase object chosen to be used
      */
     getRandomPhrase() {
-        const index = Math.floor(Math.random() * this.phrases.length);
-        const phrase = this.phrases[index];
-
-        return phrase
-    };
+        const randomNum = Math.floor(Math.random() * this.phrases.length)
+        return this.phrases[randomNum];
+    }
 
     /**
      * Begins game by selecting a random phrase and displaying it to user
      */
     startGame() {
+        const phrase = this.getRandomPhrase()
         const container = document.querySelector('.main-container');
+
         container.firstElementChild.style.display = 'none';
+        document.getElementById("overlay").style.display = "none";
+        phrase.addPhraseToDisplay()
+        this.activePhrase = phrase;
+    }
 
-        this.activePhrase = this.getRandomPhrase();
-        phrase.phrase = this.activePhrase;
-        phrase.phrase.addPhraseToDisplay();
-
-        console.log(`Active Phrase - phrase: ${this.activePhrase}`);
-    };
 
     /**
      * Checks for winning move
      * @return {boolean} True if game has been won, false if game wasn't won
      */
     checkForWin() {
-        const hide = document.querySelectorAll('.hide')
-
-        if (hide.length === 0) {
-            this.gameOver('win');
+        const show = document.querySelectorAll('.show');
+        const letters = document.querySelectorAll('.letter')
+        if (show.length === letters.length) {
+            return true;
+        } else {
+            return false;
         }
-
-        // if (phrase === 0) {
-        //     return true;
-        // } else {
-        //     return false;
-        // }
     };
 
     /**
@@ -74,14 +68,13 @@ class Game {
      * Checks if player has remaining lives and ends game if player is out
      */
     removeLife() {
-        const liveHearts = document.querySelectorAll('img');
-        const liveHeart = liveHearts[this.missed];
-        liveHeart.src = 'images/lostHeart.png';
+        const hearts = document.querySelectorAll('img');
 
-        this.missed++;
+        hearts[this.missed].src = "images/lostHeart.png";
+        this.missed += 1;
 
         if (this.missed === 5) {
-            this.gameOver('lose');
+            this.gameOver(false)
         }
     };
 
@@ -90,117 +83,73 @@ class Game {
      * @param {boolean} gameWon - Whether or not the user won the game
      */
     gameOver(gameWon) {
+        const overlay = document.getElementById("overlay")
+        const h1 = document.getElementById("game-over-message")
 
-        if (gameWon === 'lose') {
-            document.querySelector('#overlay').className = 'lose';
-            document.querySelector('#game-over-message').textContent = 'Defeat!';
+        overlay.style.display = "";
+
+        if (gameWon) {
+            overlay.className = "win";
+            h1.innerHTML = "Victory!";
         }
-
-        if (gameWon === 'win') {
-            document.querySelector('#overlay').className = 'win';
-            document.querySelector('#game-over-message').textContent = 'Victory!';
+        if (this.missed === 5) {
+            overlay.className = "lose";
+            h1.innerHTML = "Defeat!"
         }
-
-        document.querySelector('#overlay').style = 'visability: visible';
-
-        this.resetGame();
     };
+
 
     /**
      * Handles onscreen keyboard button clicks
      * @param (HTMLButtonElement) button - The clicked button element
+     * added a green border with a light green background for correct keys
+     * and a solid red border for wrong gueses.
      */
     handleInteraction(button) {
-        //put the text content of the button in a const
-        const letters = this.activePhrase.checkLetter(button.textContent)
+        const letter = button.textContent;
 
-
-        if (this.activePhrase.checkeLetter(letters)) {
+        if (this.activePhrase.checkLetter(letter)) {
             button.disabled = true;
-            letters.classList.add('chosen');
-            this.activePhrase.ShowMatchedLetter(letters)
-            this.checkForWin();
+            button.classList.add("chosen");
+            this.activePhrase.showMatchedLetter(letter);
+            this.checkForWin()
 
         } else {
-            button.disabled = false
-            letters.classList.add('wrong');
-            this.removeLife();
+            button.disabled = true;
+            button.classList.add("wrong");
+            this.removeLife()
 
-        };
+        }
 
-        if (this.checkForWin() === true) {
+        if (this.checkForWin()) {
             this.gameOver(true)
         }
-        this.resetGame();
+
     };
 
-    //create  a method that will reset your keyboard
+    //resets keyboard 
     resetKeyboard() {
         const keys = document.getElementsByClassName("key");
         for (let key of keys) {
             key.className = "key";
             key.disabled = false;
         }
-        this.resetGame();
     }
 
-    //reset the overlay you could create a method to do this.
-    resetPhrase() {
-        const ul = document.querySelector('ul');
-        const li = document.querySelectorAll('#phrase ul li');
+    //Removes the 'win' and 'lose' overlay
+    resetOverlay() {
+        const overlay = document.getElementById("overlay");
 
-        for (let i = 0; i < li.length; i++) {
-            ul.removeChild(li[i]);
-        };
-    };
-    //reset hearts you could also use a method for this
-    resetHearts() {
+        overlay.className = "start";
+        document.querySelector("h1").innerHTML = "";
+    }
+
+    //Resets the hearts
+    resetLife() {
         const hearts = document.querySelectorAll('img');
 
-        for (let i = 0; i < hearts.length; i++) {
-            hearts[i].setAttribute('src', './images/liveHeart.png');
-            this.missed = 0;
-        };
-    }
-
-    // button.disabled = true;
-    // const key = document.getElementsByClassName('.key')
-
-    // if (this.activePhrase.checkLetter(button.textContent)) {
-    //     this.activePhrase.showMatchedLetter(button.textContent);
-    //     key.classList.add('chosen');
-
-    //     if (this.checkForWin()) {
-    //         this.gameOver(true);
-    //     }
-    // } else {
-    //     key.classList.add('wrong');
-    //     this.removeLife();
-    // }
-
-    // this.resetGame();
-};
-
-// resetGame() {
-//     this.activePhrase = null;
-
-//     const ul = document.querySelector('ul');
-//     const li = document.querySelectorAll('#phrase ul li');
-//     const keys = document.getElementsByClassName('key');
-//     const hearts = document.querySelectorAll('img');
-
-//     for (let i = 0; i < li.length; i++) {
-//         ul.removeChild(li[i]);
-//     };
-
-//     for (let key of keys) {
-//         key.className = 'key';
-//         key.disabled = false;
-//     };
-
-//     for (let i = 0; i < hearts.length; i++) {
-//         hearts[i].setAttribute('src', './images/liveHeart.png');
-//         this.missed = 0;
-//     };
-//     console.log(keys)
-// };
+        hearts.forEach(heart => {
+            heart.src = "images/liveHeart.png"
+        })
+    };
+}
