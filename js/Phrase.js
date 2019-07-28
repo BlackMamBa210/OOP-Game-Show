c3bbad3f8a033933b89eb16b1c32ed21a5b55c97
Darrin Spell Jr <dspell74@gmail.com>
2019-07-27 19:09:27 -0700

    Update Phrase.js

--- js/Phrase.js
+++ js/Phrase.js
@@ -2,40 +2,36 @@
  * Project 4 - OOP Game App
  * Phrase.js */
 
+
 class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
-    }
-
+    };
     /**
      * Display phrase on game board
      */
-
     addPhraseToDisplay() {
-        const ul = document.querySelector("ul");
-        const letter = this.phrase;
-
-        for (let letter of this.phrase) {
+        const ul = document.getElementById("phrase");
+        ul.innerHTML = "";
+        const splitPhrase = this.phrase.split("");
+        splitPhrase.forEach(letter => {
+            const li = document.createElement('li');
+            ul.append(li);
             if (letter === " ") {
-                const li = document.createElement("li");
-                li.classList.add('space');
-                ul.appendChild(li);
+                li.classList.add("space");
             } else {
-                const li = document.createElement("li");
-                li.textContent = letter;
-                li.classList.add('hide', 'letter', letter);
-                ul.appendChild(li);
+                li.innerHTML = letter;
+                li.classList.add('letter', `${letter}`);
             }
-        }
-
-    };
+        });
+    }
 
     /**
      * Checks if passed letter is in phrase
      * @param (string) letter - Letter to check
      */
     checkLetter(letter) {
-        if ([...this.phrase].indexOf(letter) !== -1) {
+        if (this.phrase.includes(letter)) {
             return true;
         } else {
             return false;
@@ -46,13 +42,8 @@ class Phrase {
      * @param (string) letter - Letter to display
      */
     showMatchedLetter(letter) {
-        const list = document.querySelectorAll('li');
-
-        for (let li of list) {
-            if (li.textContent === letter) {
-                li.classList.add('show');
-                li.classList.remove('hide');
-            }
-        }
+        if (this.checkLetter(letter)) {
+            document.querySelectorAll(`.${letter}`).forEach(li => li.classList.add('show'))
+        };
     };
-};
+}
